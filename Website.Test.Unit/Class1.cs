b9c745c9c3410554657edc80db4a4354ac1dae63
alwynlombaard <alwyn.lombaard@yahoo.co.uk>
2013-11-29 14:25:22 +0000

    Playing with AutoMoq

---
+++ Website.Test.Unit/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System.Runtime.InteropServices;
+using AutoMoq;
+using NUnit.Framework;
+
+namespace Website.Test.Unit
+{
+    public class Service
+    {
+        private readonly IDependancy _dependancy;
+
+        public Service(IDependancy dependancy)
+        {
+            _dependancy = dependancy;
+        }
+
+        public string RetrieveData()
+        {
+            return _dependancy.Retrieve();
+        }
+    }
+
+    public interface IDependancy
+    {
+        string Retrieve();
+    }
+
+    [TestFixture]
+    public class ServiceTests
+    {
+        private AutoMoqer _mocker;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mocker = new AutoMoqer();
+        }
+
+        [Test]
+        public void RetrieveData_Retrieves_Hello()
+        {
+            _mocker.GetMock<IDependancy>()
+                .Setup(d => d.Retrieve())
+                .Returns("hello");
+
+            var service = _mocker.Resolve<Service>();
+
+            var result = service.RetrieveData();
+
+            Assert.That(result, Is.EqualTo("hello"));
+        }
+    }
+}
