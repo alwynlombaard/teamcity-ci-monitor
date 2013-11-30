e4bbe1df1ac64c95d4fa3cbf93902d32eecc5cbf
alwynlombaard <alwyn.lombaard@yahoo.co.uk>
2013-11-30 14:44:42 +0000

    Initial folder structure

---
+++ Website/Application.Api/Controllers/TestApiController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using website.Application.Api.Dto;
+
+namespace website.Application.Api.Controllers
+{
+    [RoutePrefix("tests")]
+    public class TestModelsController : ApiController
+    {
+        private readonly List<TestModel> _allTestModels;
+
+        public TestModelsController()
+        {
+            _allTestModels = new List<TestModel>
+            {
+                new TestModel {Id = 1, Message = "hello 1"},
+                new TestModel {Id = 2,Message = "hello 2"}
+            };
+        }
+
+        [Route("")]
+        public IEnumerable<TestModel> GetAllTestModels()
+        {
+            
+            return _allTestModels;
+        }
+
+        [Route("{id:int}")]
+        public TestModel GetById(int id)
+        {
+            return  _allTestModels
+                .FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
---
+++ Website/Application.Api/Dto/TestModel.cs
@@ -0,0 +1,8 @@
+﻿namespace website.Application.Api.Dto
+{
+    public class TestModel
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+    }
+}
