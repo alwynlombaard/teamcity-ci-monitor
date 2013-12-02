4fb40a6e4a77e3c8d3b348c73ebe0b4085d2afa7
alwynlombaard <alwyn.lombaard@yahoo.co.uk>
2013-12-02 15:09:37 +0000

    Add a theme, wip teamcity config url

--- Website/Application.Api/Controllers/AdminController.cs
+++ Website/Application.Api/Controllers/AdminController.cs
@@ -1,16 +1,46 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Security.Cryptography;
 using System.Text;
 using System.Web.Http;
+using System.Web.Mvc;
 using Ninject.Infrastructure.Language;
 using Raven.Client;
 
 namespace website.Application.Api.Controllers
 {
+    public class Configuration
+    {
+        public string TeamcityUrl { get; set; }
+    }
+
     [RoutePrefix("admin")]
     public class AdminController : ApiController
     {
+        private readonly IDocumentSession _session;
+
+        public AdminController(IDocumentSession session)
+        {
+            _session = session;
+        }
+
+        [Route("config/tc/url")]
+        public HttpStatusCodeResult PostTeamCityUrl(string uri)
+        {
+            if (string.IsNullOrEmpty(uri))
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var config = _session.Query<Configuration>()
+                .AsProjection<Configuration>()
+                .FirstOrDefault();
+            config = config ?? new Configuration();
+            config.TeamcityUrl = uri;
+            _session.Store(config);
+            return new HttpStatusCodeResult(HttpStatusCode.OK);
+        }
+
         [Route("appkeys/generate/{length:int}")]
         public string GetGeneratedKey(int length)
         {
---
+++ Website/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using Raven.Client;
+
+namespace Website.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IDocumentSession _session;
+
+        public HomeController(IDocumentSession session)
+        {
+            _session = session;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult StyleGuide()
+        {
+            return View();
+        }
+    }
+}
